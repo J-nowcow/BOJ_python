cab4a15d007fe33617b15182a8c435395a1a45f2
Jang-Hyunwoo <61009093+Jang-Hyunwoo@users.noreply.github.com>
2020-11-28 17:00:24 +0900

    Add files via upload

---
+++ 브루트 포스/[14_Bronze 2] 2798 - 블랙잭.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+ans = 0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            if num[i] + num[j] + num[k] <= M:
+                ans = max(ans, num[i] + num[j] + num[k])
+print(ans)
